8fa91b5dd11aa7223e7934a681a849b7b168b88d
Bart Wijnants <bartwijnants@outlook.com>
2017-02-04 13:02:10 +0100

    dynamic name

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class SlideEdit extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.props.onChange(this.props.slide, event.target.value);
+  }
+
   render() {
     return (
       <div>
-        <input type="text" value={this.props.slide.name} />
+        <input type="text" value={this.props.slide.name} onChange={this.handleChange} />
       </div>
     );
   }
@@ -24,7 +33,7 @@ class SlideDetails extends React.Component {
     return (
       <div>
         <SlidePreview slide={this.props.slide} />
-        <SlideEdit slide={this.props.slide} />
+        <SlideEdit slide={this.props.slide} onChange={this.props.onChange} />
       </div>
     );
   }
@@ -45,25 +54,41 @@ class SlideList extends React.Component {
 }
 
 class ReSlide extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      slides: [
+        { name: "slide1" },
+        { name: "slide2" },
+        { name: "slide3" },
+        { name: "slide4" },
+        { name: "slide5" }
+      ]
+    };
+  }
+
+  handleChange(slideToUpdate, name) {
+    const slides = this.state.slides.map(function (slide) {
+      if (slide === slideToUpdate) {
+        slide.name = name;
+      }
+      return slide;
+    });
+    this.setState({ slides: slides })
+  }
+
   render() {
     return (
       <div>
-        <SlideList slides={this.props.slides} />
-        <SlideDetails slide={this.props.slides[0]} />
+        <SlideList slides={this.state.slides} />
+        <SlideDetails slide={this.state.slides[0]} onChange={this.handleChange} />
       </div>
     );
   }
 }
 
-const SLIDES = [
-  { name: "slide1" },
-  { name: "slide2" },
-  { name: "slide3" },
-  { name: "slide4" },
-  { name: "slide5" }
-];
-
 ReactDOM.render(
-  <ReSlide slides={SLIDES} />,
+  <ReSlide />,
   document.getElementById('root')
 );
