628faa927278501a888d70152b0b4fb5838c9a89
Bart Wijnants <bartwijnants@outlook.com>
2017-02-04 19:30:06 +0100

    styling

--- src/index.js
+++ src/index.js
@@ -24,10 +24,16 @@ class Edit extends React.Component {
   render() {
     return (
       this.props.slide ?
-        <div>
-          <div><input type="text" name="text" value={this.props.slide.name} onChange={this.handleChange} /></div>
-          <div><textarea name="content" value={this.props.slide.content} onChange={this.handleChange} /></div>
-        </div>
+        <form style={{ height: '400px' }}>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input className="form-control" type="text" id="name" name="name" placeholder="Name" value={this.props.slide.name} onChange={this.handleChange} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="content">Content</label>
+            <textarea className="form-control" id="content" name="content" placeholder="Content" value={this.props.slide.content} onChange={this.handleChange} style={{ height: '200px', resize: 'non' }} />
+          </div>
+        </form>
         : null
     );
   }
@@ -37,9 +43,15 @@ class Preview extends React.Component {
   render() {
     return (
       this.props.slide ?
-        <div>
-          <div>{this.props.slide.name}</div>
-          <div><ReactMarkdown source={this.props.slide.content} escapeHtml /></div>
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">{this.props.slide.name}</h3>
+          </div>
+          <div className="panel-body" style={{height: 450 * this.props.scale + 'px'}}>
+            <div style={{ height: '400px', width: '600px', transform: 'scale(' + this.props.scale + ', ' + this.props.scale + ')', transformOrigin: '0 0'}}>
+              <ReactMarkdown source={this.props.slide.content} escapeHtml />
+            </div>
+          </div>
         </div>
         : null
     );
@@ -50,8 +62,16 @@ class Details extends React.Component {
   render() {
     return (
       <div>
-        <Preview slide={this.props.slide} />
-        <Edit slide={this.props.slide} onChange={this.props.onChange} />
+        <div className="row">
+          <div className="col-md-12">
+            <Preview slide={this.props.slide} scale="1" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <Edit slide={this.props.slide} onChange={this.props.onChange} />
+          </div>
+        </div>
       </div>
     );
   }
@@ -69,8 +89,8 @@ class ListItem extends React.Component {
 
   render() {
     return (
-      <li onClick={this.handleClick}>
-        <Preview slide={this.props.slide} />
+      <li className="list-group-item" onClick={this.handleClick}>
+        <Preview slide={this.props.slide} scale="0.5" />
       </li>
     );
   }
@@ -79,7 +99,7 @@ class ListItem extends React.Component {
 class List extends React.Component {
   render() {
     return (
-      <ul>
+      <ul className="list-group">
         {this.props.slides.map((slide) =>
           <ListItem key={slide.id} slide={slide} onSelect={this.props.onSelect} />
         )}
@@ -92,8 +112,14 @@ class SideBar extends React.Component {
   render() {
     return (
       <div>
-        <List slides={this.props.slides} onSelect={this.props.onSelect} />
-        <button onClick={this.props.onAdd}>Add</button>
+        <div className="row" style={{ maxHeight: '700px', height: '700px', overflowY: 'scroll' }}>
+          <List slides={this.props.slides} onSelect={this.props.onSelect} />
+        </div>
+        <div className="row" style={{ height: '100px' }}>
+          <div className="col-md-12">
+            <button className="btn btn-default" onClick={this.props.onAdd}>New slide</button>
+          </div>
+        </div>
       </div>
     );
   }
@@ -143,9 +169,13 @@ class ReSlide extends React.Component {
 
   render() {
     return (
-      <div>
-        <SideBar style={this.sideBarStyle} slides={this.state.slides} onAdd={this.handleAdd} onSelect={this.handleSelect} />
-        <Details slide={this.state.selectedSlide} onChange={this.handleChange} />
+      <div className="row">
+        <div className="col-md-3">
+          <SideBar style={this.sideBarStyle} slides={this.state.slides} onAdd={this.handleAdd} onSelect={this.handleSelect} />
+        </div>
+        <div className="col-md-9">
+          <Details slide={this.state.selectedSlide} onChange={this.handleChange} />
+        </div>
       </div>
     );
   }
